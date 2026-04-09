1fa2b4831962e159c3a346c0fe0fd29abcc83672
Renkas Vladislav <Renkasvv@my.msu.ru>
2026-04-09 20:57:28 +0300

    Copy previous task 1

---
+++ 20260406/2/mood/common/constants.py
@@ -0,0 +1,15 @@
+"""Shared constants for MOOD."""
+
+DEFAULT_HOST = "127.0.0.1"
+DEFAULT_PORT = 1337
+FIELD_SIZE = 10
+MONSTER_MOVE_INTERVAL = 30.0
+WELCOME_TEXT = "<<< Welcome to MOOD 0.1 >>>"
+SHELL_PROMPT = "(mood) "
+
+WEAPONS = {
+    "sword": 10,
+    "spear": 15,
+    "axe": 20,
+    "FIRE SWORD": 111,
+}
